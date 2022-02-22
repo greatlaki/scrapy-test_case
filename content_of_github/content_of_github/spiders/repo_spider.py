55da82704f64ce94ba70023bc2362e759c86386d
greatlaki <greatlaki911@gmail.com>
2022-02-22 20:02:10 +0300

    I fixed  my mistakes, but so far the spider is collecting information from one repository

--- content_of_github/content_of_github/spiders/repo_spider.py
+++ content_of_github/content_of_github/spiders/repo_spider.py
@@ -1,6 +1,10 @@
 import scrapy
 
-from const_of_css import *
+NAME ="li.Box-row:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > h3:nth-child(1) > a:nth-child(1)"
+ABOUT = "li.Box-row:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > p:nth-child(2)"
+LINK = "li.Box-row:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > h3:nth-child(1) > a::attr(href)"
+STARS = "li.Box-row:nth-child(1) > div:nth-child(1) > div:nth-child(2) > a:nth-child(3)"
+EX_LINK = "li.js-responsive-underlinenav-item:nth-child(2) > a::attr(href)"
 
 
 class GitSpider(scrapy.Spider):
@@ -8,14 +12,14 @@ class GitSpider(scrapy.Spider):
     start_urls = ["https://github.com/scrapy"]
 
     def parse(self, response):
-        link_repo = "li.js-responsive-underlinenav-item:nth-child(2) > a::attr(href)"
-        for link in response.css(link_repo.get(-1)):
+        for link in response.css(EX_LINK):
             yield response.follow(link, callback=self.parse_repo)
 
     def parse_repo(self, response):
         yield {
-            "name": response.css(NAME).get(-1).split('/')[-1],
-            "about": response.css(ABOUT.get(-1).split("\n")[-2].strip()),
-            "link": response.css(LINK.get(-1)),
-            "stars": response.css(STARS.get(-1).split("\n")[-2].strip()),
+            "name": response.css(NAME).get(-1).split('\n')[-2].strip(),
+            "about": response.css(ABOUT).get(-1).split("\n")[-2].strip(),
+            "link": response.css(LINK).get(-1),
+            "stars": response.css(STARS).get().split("\n")[-2].strip(),
         }
+
